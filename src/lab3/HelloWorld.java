9d43e08352147cb273f4d32b8f944bdcd6990660
Baejwan <gamegus1996@gmail.com>
2019-08-01 15:52:37 +0700

    Initial commit

---
+++ src/lab3/HelloWorld.java
@@ -0,0 +1,54 @@
+package lab3;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+
+    System.out. prln ( ''Hell, world !!!! ");
+     System.out.prlntln(''ชื่อ นางเบญจพรรณ  ดำชุม" );
+
+      Stem.out.prlntln(รหัสนักศึกษา 3622117600028");
+
+ system .out .print ln
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/lab5/ex1.java
@@ -0,0 +1,9 @@
+package lab5;
+
+public class ex1 {
+
+
+
+
+
+}
